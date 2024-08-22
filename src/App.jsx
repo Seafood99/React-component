490f049aac5a1a63f5f87d1a928f775eb9839201
Dawam <muhammadawamali@gmail.com>
2024-08-22 19:53:54 +0700

    nambah roting ke recipes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home"
+import Recipes from "./pages/Recipes"
+import RecipesNeu from './pages/RecipesNeu';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Recipes" element={<Recipes />} />
+        <Route path="/recipes-neu" element={<RecipesNeu />} />
       </Routes>
     </BrowserRouter>
   )
