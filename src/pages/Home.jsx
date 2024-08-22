9409f4c3fa8aca2944f96766da3e484d567fcd39
Dawam <muhammadawamali@gmail.com>
2024-08-22 18:45:23 +0700

    membuat halaman pricecard menggunakan map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,59 @@
+import React from 'react';
+import PriceCard from '../component/PriceCard';
+
 export default function Home() {
+
+    const priceCards = [
+        {
+            price: '19',
+            title: 'Starter',
+            description: 'Unleash the power of automation.',
+            points: [
+                "Multi-step-zip",
+                "3 Premium Apps",
+                "2 User Teams"
+            ]
+        },
+        {
+            price: '54',
+            title: 'Profesional',
+            description: 'Automation tools to take your work to the next level.',
+            points: [
+                "Multi-step-zip",
+                "Unlimited Premium",
+                "50 Share Teams",
+                "Shared Workspace"
+            ]
+        },
+        {
+            price: '89',
+            title: 'Company',
+            description: 'Automation plus enterprise-grade features.',
+            points: [
+                "Unlimited Premium",
+                "Unlimited User Teams",
+                "Advanced Admin",
+                "Custom Data Retention"
+            ]
+        }
+    ];
+
     return (
-        <h1>Home</h1>
-    )
+        <div className='container'>
+            <p>Home</p>
+
+            <div className='grid grid-cols-3 gap-3'>
+                {
+                priceCards.map((card, index) => (
+                    <PriceCard
+                        key={index}
+                        price={card.price}
+                        title={card.title}
+                        description={card.description}
+                        points={card.points}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
